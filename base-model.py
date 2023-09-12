a4aa7f637d3aafc18fc6a309d7dbecb72d896b2f
Chenghong Yang <c.yang2@students.uu.nl>
2023-09-13 00:56:15 +0200

    Use the bow as way to vectorize

--- base-model.py
+++ base-model.py
@@ -1,6 +1,6 @@
 import sklearn
 import numpy as np
-import pandas as pd
+
 
 
 
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from sklearn.linear_model import RidgeClassifier
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+
+stp_words = stopwords.words('english')
 
 def load_data(filename):
     with open(filename) as f:
@@ -17,35 +20,55 @@ def load_data(filename):
         message = line.lstrip(label)
         messages.append(message)
         labels.append(label)
-    vectorizer = TfidfVectorizer(
-        sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english"
-    )
+    #vectorizer = TfidfVectorizer(
+    #    sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english"
+    #)
     df= pd.DataFrame(list(zip(labels,messages)),columns =['Label',"Text"])
     #Act_Dict : {"ack" : 0, "affirm" : 1, "bye" : 2, "confirm" : 3 , "deny" : 4, "hello" : 5, "inform" : 6, "negate" : 7, "null" : 8, "repeat" : 9, "null" : 10, "repeat": 11, "reqalts" : 12, "reqmore" : 13, "request" : 14, "restart" : 15, "thankyou" : 16}
-    vectorList = vectorizer.fit_transform(messages)
-    df["Vector"]=vectorList
-    print(vectorList)
+
+    #print(df)
+    #print(vectorList)
     train, test = sklearn.model_selection.train_test_split(df, test_size=0.2)
-    print(test)
+    words_dict = {} #A dict contains all the words in train data
+    i = 0
+
+    train_text = train['Text'].values.tolist()
+
+    for line in train_text:
+        for word in line.split():
+            if word not in words_dict.keys():
+                if word not in stp_words:
+                    words_dict[word]=i
+                    i += 1
+    vec_train = []
+    for line in train_text:
+        vec = np.zeros(len(words_dict.keys()))
+        for word in line.split():
+            if word not in stp_words:
+                vec[words_dict[word]] += 1
+        vec_train.append(vec)
+    train['Vector'] = vec_train
+
+    test_text = test['Text'].values.tolist()
+    vec_test = []
+    for line in test_text:
+        vec = np.zeros(len(words_dict.keys()))
+        for word in line.split():
+            if word in words_dict.keys():
+                if word not in stp_words:
+                    vec[words_dict[word]] += 1
+        vec_test.append(vec)
+    test['Vector'] = vec_test
+
     return(train,test,df["Label"].unique())
+
+
 
 
 filename = "dialog_acts.dat"
 train,test,targets = load_data(filename)
 X_train,y_train,X_test,y_test = train["Vector"],train["Label"],test["Vector"],test["Label"]
 
-clf = RidgeClassifier(tol=1e-2, solver="sparse_cg")
-clf.fit(X_train, y_train)
-pred = clf.predict(X_test)
-
-
-fig, ax = plt.subplots(figsize=(10, 5))
-ConfusionMatrixDisplay.from_predictions(y_test, pred, ax=ax)
-ax.xaxis.set_ticklabels(targets)
-ax.yaxis.set_ticklabels(targets)
-_ = ax.set_title(
-    f"Confusion Matrix for Restaurant Dialog Classifier"
-)
 '''
 
 df=pd.read_csv('dialog_acts.dat',sep=' ')
