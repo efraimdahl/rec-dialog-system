3228de5657f55540442ee6115a1ad3c58d2b7e3e
Zeijden <r.j.vanderzeijden@uu.nl>
2023-09-14 00:26:51 +0200

    Baseline models implemented (keyword matching and most frequent label)

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.linear_model import RidgeClassifier
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.feature_extraction.text import TfidfVectorizer
+import baseline_model
+import sklearn.dummy
 
 def load_data(filename):
     with open(filename) as f:
@@ -19,33 +21,62 @@ def load_data(filename):
         messages.append(message)
         labels.append(label)
         limit+=1
-    vectorizer = TfidfVectorizer(
-        sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english"
-    )
     df= pd.DataFrame(list(zip(labels,messages)),columns =['Label',"Text"])
-    train, test = sklearn.model_selection.train_test_split(df, test_size=0.2)
-    X_train = vectorizer.fit_transform(train["Text"])
-
-    # Extracting features from the test data using the same vectorizer
-    X_test = vectorizer.transform(test["Text"])
-    y_train = train["Label"]
-    y_test = test["Label"]
-    feature_names = vectorizer.get_feature_names_out()
-    target_names = df['Label'].unique()
-    return(X_train, X_test,y_train, y_test,feature_names, target_names)
+    return df
 
 
 filename = "dialog_acts.dat"
-X_train, X_test, y_train, y_test, feature_names, target_names = load_data(filename)
+df = load_data(filename)
 
-clf = RidgeClassifier(tol=1e-2, solver="sparse_cg")
-clf.fit(X_train, y_train)
-pred = clf.predict(X_test)
-print(target_names)
+train, test = sklearn.model_selection.train_test_split(df, test_size=0.2)
+vectorizer = TfidfVectorizer(
+    sublinear_tf=True, max_df=0.5, min_df=5, stop_words="english"
+)
+
+X_train = vectorizer.fit_transform(train["Text"])
+# Extracting features from the test data using the same vectorizer
+X_test = vectorizer.transform(test["Text"])
+y_train = train["Label"]
+y_test = test["Label"]
+feature_names = vectorizer.get_feature_names_out()
+target_names = df['Label'].unique()
+
+# Most frequent baseline
+
+freqmodel = sklearn.dummy.DummyClassifier(strategy="most_frequent")
+freqmodel.fit(X_train,y_train)
+pred_freq = freqmodel.predict(X_test)
+print(freqmodel.score(X_test, y_test))
+
+# Keyword matching baseline
+
+keywordmodel = baseline_model.KeywordClassifier()
+pred_keyword = keywordmodel.predict(test["Text"])
+print(keywordmodel.score(test["Text"],y_test))
+
+# # Ridge classifier
+# clf = RidgeClassifier(tol=1e-2, solver="sparse_cg")
+# clf.fit(X_train, y_train)
+# pred = clf.predict(X_test)
+
+
+# fig, ax = plt.subplots(figsize=(10,10))
+# ConfusionMatrixDisplay.from_predictions(y_test, pred_freq, ax=ax,labels=target_names)
+# _ = ax.set_title(
+#     f"Confusion Matrix using Most Frequent model"
+# )
+# fig.savefig("Most frequent model Confusion Matrix.png")
 
 fig, ax = plt.subplots(figsize=(10,10))
-ConfusionMatrixDisplay.from_predictions(y_test, pred, ax=ax,labels=target_names)
+ConfusionMatrixDisplay.from_predictions(y_test, pred_keyword, ax=ax,labels=target_names)
 _ = ax.set_title(
-    f"Confusion Matrix for Restaurant Dialog Classifier"
+    f"Confusion Matrix using Keyword Matching model"
 )
-fig.savefig("Base Confusion Matrix.png")
+fig.savefig("Keyword model Confusion Matrix.png")
+
+# fig, ax = plt.subplots(figsize=(10,10))
+# ConfusionMatrixDisplay.from_predictions(y_test, pred, ax=ax,labels=target_names)
+# _ = ax.set_title(
+#     f"Confusion Matrix using Ridge classifier"
+# )
+# fig.savefig("Ridge model Confusion Matrix.png")
