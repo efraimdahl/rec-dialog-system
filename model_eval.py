78f476b9dca25be3b5a9e977efb26c263a16c7ad
Chenghong Yang <c.yang2@students.uu.nl>
2023-09-14 00:35:24 +0200

    Classifiers implementation, bugs fix

--- model_eval.py
+++ model_eval.py
@@ -1,16 +1,19 @@
 import pickle
-from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import RidgeClassifier, LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
 for mode in ["complete","dedupl"]:
     file1 = open("data/"+mode+"/X_test.pickle", 'rb')
     file2 = open("data/"+mode+"/y_test.pickle", 'rb')
-    file3 = open("data/"+mode+"/target_names.pickle", 'rb')
+    file3 = open("data/"+"complete"+"/target_names.pickle", 'rb')
     X_test=pickle.load(file1)
     y_test=pickle.load(file2)
     target_names=pickle.load(file3)
-    for model in ["Ridge"]:
+    for model in ["Ridge","KNN", "DecisionTree"]:
         modelfile = open("models/"+mode+"/"+model+".pkl",'rb')
         pickled_model = pickle.load(modelfile)
         pred = pickled_model.predict(X_test)
