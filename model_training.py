a8df424ea32a4a04d488ec576dc67f507bc3f516
JuulPetit <juul@fampetit.nl>
2023-09-14 15:45:43 +0200

    added baseline models

--- model_training.py
+++ model_training.py
@@ -3,6 +3,8 @@ from sklearn.linear_model import RidgeClassifier, LinearRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.dummy import DummyClassifier
+from baseline_model import KeywordClassifier
 
 for mode in ["complete","dedupl"]:
     file1 = open("data/"+mode+"/X_train.pickle", 'rb')
@@ -31,6 +33,15 @@ for mode in ["complete","dedupl"]:
             pass
             # dim = len(X_train[0])
             # clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes = (dim, 17), random_state = 1)
+        
+        if model == "most_frequent":
+            clf = DummyClassifier(strategy="most_frequent")
+            clf.fit(X_train, y_train)
+
+        if model == "keyword":
+            clf = KeywordClassifier()
+            
+
         outfile = open("models/"+mode+"/"+model+".pkl",'wb')
         pickle.dump(clf,outfile)
 
