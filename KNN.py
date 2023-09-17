a65251e677cda192e90eb4ba84803f059d30845e
Matthew Scheeres <46484495+MatthewScheeres@users.noreply.github.com>
2023-09-17 18:42:09 +0200

    Removed unused knn.py file

--- KNN.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import make_blobs
-from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
-from sklearn.model_selection import train_test_split
-from matplotlib.colors import ListedColormap
-
-data = make_blobs(n_samples = 1000, centers = 100, random_state = 8)
-print(data)
-
