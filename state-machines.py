d94b9fd20f01b21345161a262b052d71f7a3c029
efraimdahl <efraimdahl@uchicago.edu>
2023-09-22 14:18:36 +0200

    added some polishes for request changes, so that they are more consistently recognized

--- state-machines.py
+++ state-machines.py
@@ -33,6 +33,9 @@ class RestaurantAgent(StateMachine):
         return_restaurant.to(completed, cond="exit_conversation")
         | return_restaurant.to(give_information, unless="exit_conversation")
     )
+    reprovide_information = (
+        give_information.to(give_information)
+    )
     
     complete_process = (give_information.to(completed, cond="exit_conversation")
                         | give_information.to(return_restaurant, unless="exit_conversation")
@@ -53,6 +56,8 @@ class RestaurantAgent(StateMachine):
         self.all_restaurants = pd.read_csv(restaurant_file)
         self.filteredRestaurants = None
         self.parser = TextParser(classifier_file,restaurant_file,vectorizer_file)
+        self.current_suggestion = None
+        self.current_suggestion_set = False
         super(RestaurantAgent, self).__init__(rtc=False)
     
     # HELPER FUNCTIONS
@@ -75,7 +80,15 @@ class RestaurantAgent(StateMachine):
         if(self.foodType!="dontcare"):
             df=df[df["food"]==self.foodType]
         return df
-       
+
+    #This is to only mention specified information in the response.
+    def no_response_formater(self,other=False):
+        foodpart = f'serving {self.foodType} food' if (self.foodType!="" and self.foodType!="dontcare") else ""
+        areapart = f'in the {self.area}' if (self.area!="" and self.area!="dontcare") else ""
+        pricepart = f'that has {self.priceRange} prices' if (self.priceRange!="" and self.priceRange!="dontcare") else ""
+        otherp = 'other' if(other) else '' 
+        resp = f"I'm sorry but there is no {otherp} restaurant {foodpart} {areapart} {pricepart}"
+        return resp
         
     #CONDITIONAL TRANSITIONS
     def input_received(self,input):
@@ -86,7 +99,8 @@ class RestaurantAgent(StateMachine):
         return self.area != "" and self.foodType!="" and self.priceRange != ""
     
     def exit_conversation(self, input):
-        return self.parser.parseText(input)[0]=="bye"
+        exit_input = self.parser.parseText(input)[0]
+        return exit_input=="bye" or exit_input=="thankyou"
     
     def preference_change(self,input):
         #print(input)
@@ -100,6 +114,7 @@ class RestaurantAgent(StateMachine):
                 self.processVariableDict(classAnswer[1])
 
     def on_enter_waiting_for_input(self):
+        #print("Waiting for input", self.counter)
         if(self.counter>0):
             if(self.area==""):
                 print("What part of town do you have in mind?")
@@ -113,13 +128,12 @@ class RestaurantAgent(StateMachine):
             else:
                 self.context = None
                 print("I did not understand your last input, can we try again?")
-
         self.counter+=1
         
 
     def on_enter_hello(self):
         print("Hello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?")
-        
+        self.send("start_processing")
     def on_enter_provide_information(self, input):
         request_type = self.parser.parseText(input,context=self.context,requestPossible=True)
         #print(input, request_type)
@@ -129,56 +143,76 @@ class RestaurantAgent(StateMachine):
         if(self.filteredRestaurants is None):
             self.filteredRestaurants = self.search_restaurant()
             if (len(self.filteredRestaurants) == 0):
-                print(f"I'm sorry but there is no restaurant serving {self.foodType} food")
-                # this is placeholder (it breaks)
+                resp = self.no_response_formater()
+                print(resp)
+                self.current_suggestion = None
+                self.filteredRestaurants=None
+                self.current_suggestion_set = False
                 return
             row = self.filteredRestaurants.iloc[0]
             self.current_suggestion = row
+            self.current_suggestion_set = True
             self.tries=1
         else:
             if(len(self.filteredRestaurants) <= self.tries + 1):
-                print(f"I'm sorry but there is no restaurant serving {self.foodType} food")
+                resp = self.no_response_formater(other="true")
+                print(resp)
+                self.current_suggestion = None
+                self.filteredRestaurants=None
+                self.current_suggestion_set = False
                 return
             row = self.filteredRestaurants.iloc[self.tries]
             self.current_suggestion = row
+            self.current_suggestion_set = True
             self.tries+=1
         print(f"{row['restaurantname']} is a nice place in the {row['area']} part of town serving {row['food']} food and the prices are {row['pricerange']}")
         
+    #There are three possibilities, the user can request an alternative updatin
     def on_enter_give_information(self, input):
-        request_type = self.parser.parseText(input, context=self.context, requestPossible=True)
-        
-        response_dict = {
-            "phone": "phone number",
-            "postcode": "postcode",
-            "addr": "address",
-            "food": "food served",
-            "pricerange": "price range",
-            "restaurantname": "name",
-            "area": "area"
-        }
-        response = ""
-        
-        for key in request_type[1]:
-            if key in response_dict:
-                attribute = response_dict[key]
-                value = self.current_suggestion[key]
-                response += f"the {attribute} of {self.current_suggestion['restaurantname']} is {value}, " if not response else f"their {attribute} is {value}, "
-        
-       # if response:
-       #    print(response.capitalize()[:-2] + ".")
+        requestPossible = self.current_suggestion_set #Only enable information requests if a restaurant is loaded
+        request_type = self.parser.parseText(input, context=self.context, requestPossible=requestPossible)
+        print(request_type,self.current_suggestion)
+        if(self.current_suggestion_set):
+            response_dict = {
+                "phone": "phone number",
+                "postcode": "postcode",
+                "addr": "address",
+                "food": "food served",
+                "pricerange": "price range",
+                "restaurantname": "name",
+                "area": "area"
+            }
+            response = ""
+            for key in request_type[1]:
+                if key in response_dict:
+                    attribute = response_dict[key]
+                    value = self.current_suggestion[key]
+                    response += f"the {attribute} of {self.current_suggestion['restaurantname']} is {value}, " if not response else f"their {attribute} is {value}, "
+            
+            if response !="" : 
+                print(response.capitalize()[:-2] + ".")
+                return
+            print("Can you provide specific information you are looking for such as phone number, area or address?")
+        else:
+            if(type(request_type[1])==dict):
+                self.send("request_alternative", input=input)
+                return
+            print("Sorry, can you try changing the area, price or foodtype?")
             
     def on_enter_complete_process(self):
         print("Thank you for using the Cambridge restaurant system. Goodbye!")
 
     def on_request_alternative(self,input):
+        print("updated preferences", input)
         if (self.preference_change(input)):
-            preferences = self.parser.parseText(input)[1]
+            preferences = self.parser.parseText(input,requestPossible=False)[1]
             self.processVariableDict(preferences)
             self.filteredRestaurants = None
             self.tries = 0
 
     # INPUT HANDLING
     def input_step(self, user_input: str) -> str:
+        print(self.current_state)
         input = self.parser.parseText(user_input)
         #print(f"Before state: {self.current_state}")
         print(f"User message: {user_input}")
@@ -188,13 +222,15 @@ class RestaurantAgent(StateMachine):
             self.send("receive_input", input=user_input)
             self.send("evaluate_input")
         elif self.current_state.id == "return_restaurant":
-            if (input[0] in ["inform","reqalts"]):
+            if (input[0] in ["inform","reqalts","negate","confirm"] or (not self.current_suggestion_set and input[0]=="request")):
                 self.send("request_alternative", input=user_input)
             else:
                 self.send("provide_information", input=user_input)
         elif self.current_state.id == "give_information":
-            if (input[0] in ["inform","reqalts"]):
+            if (input[0] in ["inform","reqalts","negate","confirm"]):
                 self.send("request_alternative", input=user_input)
+            elif (input[0] in ["request"]):
+                self.send("reprovide_information", input=user_input)
             else:
                 self.send("complete_process", input=user_input)
         
@@ -208,6 +244,7 @@ class RestaurantAgent(StateMachine):
         """
         Save a graph of the state machine with the current state highlighted to specified file
         """
+        return ""
         graph = DotGraphMachine(self)
         if (filename != ""):
             graph().write_png(filename)
@@ -220,6 +257,7 @@ def main():
     vectorizer = pkl.load(open("./ass_1a/models/complete/vectorizer.pkl",'rb'))
     restaurant_file = "restaurant_info.csv"
     sm = RestaurantAgent(restaurant_file,classifier,vectorizer)
+    """
     sm.graph("initial.png")
     
     #print(sm.current_state)
@@ -237,7 +275,7 @@ def main():
     #sm.input_step("Goodbye")
 
     print(sm.completed.is_active)
-
+    """
     # User input loop
     while not sm.completed.is_active:
         user_input = input("Type your response: ")
